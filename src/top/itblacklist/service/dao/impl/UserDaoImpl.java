3b98424d5cc5064f47a6c7031eeac35d863c9406
SNake <snake_goodluck@hotmail.com>
2018-09-19 23:44:49 +0800

    spring+dbcp连接池

--- src/top/itblacklist/service/dao/impl/UserDaoImpl.java
+++ src/top/itblacklist/service/dao/impl/UserDaoImpl.java
@@ -1,11 +1,32 @@
 package top.itblacklist.service.dao.impl;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import top.itblacklist.service.dao.IUserDao;
+
+import javax.sql.DataSource;
+import java.sql.Connection;
+import java.sql.PreparedStatement;
 
 public class UserDaoImpl implements IUserDao {
 
+    private DataSource dataSource;
+
+    public void setDataSource(DataSource dataSource) {
+        this.dataSource = dataSource;
+    }
+
     @Override
     public void save() {
-        System.out.println("我保存了");
+        System.out.println("保存方法");
+        try{
+            Connection conn = dataSource.getConnection();
+            PreparedStatement ps = conn.prepareStatement("insert into user(username) values (?)");
+            ps.setString(1, "zhangsan");
+            ps.executeUpdate();
+
+        }catch (Exception e){
+            e.printStackTrace();
+        }
+
     }
 }
